7ae4b731f40525246487f555c4be09e7aaac0faf
sultanankrasavchik68 <sultananvarov34@gmail.com>
2025-04-07 22:31:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const array = [2, 4, 9, 10, 45, 67, 8, 90];
+console.log(array, 'boshlanishdagi array'); 
+
+let  evenNumbers = array.filter(num => num % 2 == 0);
+console.log(evenNumbers, 'juft sonlarga ajratilgan array'); 
